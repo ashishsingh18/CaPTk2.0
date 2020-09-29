ee452ee6e433cfc502b709e47a956cffa4e24460
Ashish Singh <ashish.singh@uphs.upenn.edu>
2020-09-29 15:39:50 -0400

    adding files, #151

---
+++ Plugins/upenn.cbica.captk.brain.pseudoprogression/files.cmake
@@ -0,0 +1,46 @@
+set(SRC_CPP_FILES
+  
+)
+
+set(INTERNAL_CPP_FILES
+  captkPseudoProgressionPluginActivator.cpp
+  QcaptkPseudoProgressionView.cpp
+  #Common/QmitkDataSelectionWidget.cpp
+)
+
+set(UI_FILES
+  src/internal/QcaptkPseudoProgressionControls.ui
+)
+
+set(MOC_H_FILES
+  src/internal/captkPseudoProgressionPluginActivator.h
+  src/internal/QcaptkPseudoProgressionView.h
+  #src/internal/Common/QmitkDataSelectionWidget.h
+)
+
+set(CACHED_RESOURCE_FILES
+  resources/icon.svg
+  plugin.xml
+)
+
+set(QRC_FILES
+  resources/captkPseudoProgression.qrc
+)
+
+set(CPP_FILES)
+
+foreach(file ${SRC_CPP_FILES})
+  set(CPP_FILES ${CPP_FILES} src/${file})
+endforeach(file ${SRC_CPP_FILES})
+
+#usFunctionEmbedResources(
+#CPP_FILES
+# LIBRARY_NAME "liborg_mitk_gui_qt_multilabelsegmentation"
+#ROOT_DIR resources
+#FILES Interactions/SegmentationInteraction.xml
+#      Interactions/ConfigSegmentation.xml
+#)
+
+foreach(file ${INTERNAL_CPP_FILES})
+  set(CPP_FILES ${CPP_FILES} src/internal/${file})
+endforeach(file ${INTERNAL_CPP_FILES})
